e026c3b8559b9b99f4dcdadfe343783c78dc8d95
Rudolf Schmidt <me@rudionrails.com>
2011-09-15 11:27:21 +0200

    initial import

---
+++ lib/capistrano-strategy-copy-bundled.rb
@@ -0,0 +1,11 @@
+require "capistrano-strategy-copy-bundled/version"
+
+module Capistrano
+  module Strategy
+    module Copy
+      module Bundled
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/capistrano-strategy-copy-bundled/version.rb
@@ -0,0 +1,9 @@
+module Capistrano
+  module Strategy
+    module Copy
+      module Bundled
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
