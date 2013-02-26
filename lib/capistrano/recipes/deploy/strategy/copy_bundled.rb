b9bf29811c4c3eab7c898e1cfaa33f7013b1dedd
Tom Meier <tom@venombytes.com>
2013-02-26 16:25:18 +1100

    Revert "Allow copy cache directory to be recopied to staging area after bundling in cache directory"
    This reverts commit 5b00beff01c2678930a796ea36b4dd6c12b91915.

--- lib/capistrano/recipes/deploy/strategy/copy_bundled.rb
+++ lib/capistrano/recipes/deploy/strategy/copy_bundled.rb
@@ -19,20 +19,20 @@ module Capistrano
         def deploy!
           logger.info "running :copy_bundled strategy"
 
-          if copy_cache
-            run_copy_cache_strategy
-            bundle!(:with_cache)
-          else
-            run_copy_strategy
-            bundle!
-          end
+          copy_cache ? run_copy_cache_strategy : run_copy_strategy
 
           create_revision_file
+
+          configuration.trigger('strategy:before:bundle')
+          #Bundle all gems
+          bundle!
+          configuration.trigger('strategy:after:bundle')
 
           logger.info "compressing repository"
           configuration.trigger('strategy:before:compression')
           compress_repository
           configuration.trigger('strategy:after:compression')
+
 
           logger.info "distributing packaged repository"
 
@@ -45,37 +45,25 @@ module Capistrano
 
         private
 
-        def bundle!(with_cache = false)
-          configuration.trigger('strategy:before:bundle')
+        def bundle!
+          bundle_cmd      = configuration.fetch(:bundle_cmd, "bundle")
+          bundle_gemfile  = configuration.fetch(:bundle_gemfile, "Gemfile")
+          bundle_dir      = configuration.fetch(:bundle_dir, 'vendor/bundle')
+          bundle_flags    = configuration.fetch(:bundle_flags, "--deployment --quiet")
+          bundle_without  = [*configuration.fetch(:bundle_without, [:development, :test])].compact
+          bundle_cache    = copy_cache ? copy_cache : destination
 
-          bundle_cmd        = configuration.fetch(:bundle_cmd, "bundle")
-          bundle_gemfile    = configuration.fetch(:bundle_gemfile, "Gemfile")
-          bundle_dir        = configuration.fetch(:bundle_dir, 'vendor/bundle')
-          bundle_flags      = configuration.fetch(:bundle_flags, "--deployment --quiet")
-          bundle_without    = [*configuration.fetch(:bundle_without, [:development, :test])].compact
-          bundle_cache_dir  = with_cache ? copy_cache : destination
-
-          args = ["--gemfile #{File.join(bundle_cache_dir, bundle_gemfile)}"]
+          args = ["--gemfile #{File.join(bundle_cache, bundle_gemfile)}"]
           args << "--path #{bundle_dir}" unless bundle_dir.to_s.empty?
           args << bundle_flags.to_s unless bundle_flags.to_s.empty?
           args << "--without #{bundle_without.join(" ")}" unless bundle_without.empty?
 
           Bundler.with_clean_env do
-            logger.info "bundling gems to directory : #{bundle_cache_dir}..."
-            run_locally "cd #{bundle_cache_dir} && #{bundle_cmd} install #{args.join(' ').strip}"
+            logger.info "installing gems to local cache : #{destination}..."
+            run_locally "cd #{destination} && #{bundle_cmd} install #{args.join(' ').strip}"
 
-            logger.info "packaging gems for bundler in #{bundle_cache_dir}..."
-            run_locally "cd #{bundle_cache_dir} && #{bundle_cmd} package --all"
-          end
-
-          copy_bundled_cache! if with_cache
-
-          configuration.trigger('strategy:after:bundle')
-        end
-
-        def copy_bundled_cache!
-          execute "copying additional bundled cache to deployment staging area #{destination}" do
-            Dir.chdir(copy_cache) { copy_files(queue_files) }
+            logger.info "packaging gems for bundler in #{destination}..."
+            run_locally "cd #{destination} && #{bundle_cmd} package --all"
           end
         end
       end
